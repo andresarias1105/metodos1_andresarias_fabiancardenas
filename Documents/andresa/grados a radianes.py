12e98b5f89b97b55f4c27bd74b31646155ee59f1
andresarias1105 <123577190+andresarias1105@users.noreply.github.com>
2023-01-27 11:11:13 -0500

    Delete grados a radianes.py

--- Documents/andresa/grados a radianes.py
+++
@@ -1,8 +0,0 @@
-def gr_a_r(grados:float)->float:
-    return grados*3.1416/180
-
-grados = float(input('ingrese valor a convertir'))
-
-radianes =round( gr_a_r(grados),2)
-
-print(grados,' grados son ',radianes, ' radianes')
